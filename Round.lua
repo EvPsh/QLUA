31142d626429e3813e566cbbbc45d46fc8380bc6
Evgeniy <98161398+EvPsh@users.noreply.github.com>
2022-11-03 22:22:25 +0300

    Create Round.lua

---
+++ Round.lua
@@ -0,0 +1,12 @@
+function round(number, znaq) -- функция округления числа num до знаков znaq
+local num = tonumber(number)
+local idp = tonumber(znaq)
+
+	if num then
+		local mult = 10 ^ (idp or 0)
+		if num >= 0 then return math.floor(num * mult + 0.5) / mult
+		else return math.ceil(num * mult - 0.5) / mult
+		end
+	else return num
+	end
+end
