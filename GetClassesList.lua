9e4fef67ccb8474fa92eb8f1446e60494cca7234
EvPsh <faradoxe@bk.ru>
2023-02-07 22:24:40 +0300

    Small changes

--- GetClassesList.lua
+++ GetClassesList.lua
@@ -1,11 +1,26 @@
 function main()
+-- Справочная информация:
 -- Вывод списка классов Quik
+-- вывод инструментов класса
+-- вывод справочных данных о классе в формате ключ(поле таблицы) = значение
+-- вывод информации об инструменте в формате ключ(поле таблицы) = значение
 ---
+
 local list = getClassesList() 
+	message("list: \n" .. tostring(list))
 
--- string sec_list = getClassSecurities("SPBFUT")  
--- TABLE getClassInfo (STRING)
+local sec_list = getClassSecurities("SPBFUT") -- вывод инструментов класса SPBFUT (Sec_code)
+	message("Sec_list: \n" .. tostring(sec_list))
 
-message("list: " .. tostring(list))
+local tInstr = {}
+tInstr = getClassInfo("SPBFUT") -- вывод справочной информации из класса SPBFUT
+	for key, v in pairs (tInstr) do
+	   message(tostring (key) .. " = " .. tostring(v), 1)
+	end
 
+local info = {}
+info = getSecurityInfo("SPBFUT", "RMH3") -- вывод информации об инструменте RMH3 из класса SPBFUT
+	for key, v in pairs(info) do
+	   message(tostring (key) .. " = " .. tostring(v), 2)
+	end
 end
